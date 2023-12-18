90fab19248f36d294f9cf6e727c409a630b35dda
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-18 10:49:27 +0700

    5.7 bloglist frontend step7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+export default function Blog({ blog }) {
+	const [showDetails, setShowDetails] = useState(false);
+
+	const styles = {
+		paddingTop: 10,
+		paddingBottom: 10,
+		paddingLeft: 10,
+		border: 'solid',
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
+	return (
+		<div style={styles}>
+			<div>
+				<span>
+					{blog.title} {blog.author}
+				</span>
+				<button type='button' onClick={() => setShowDetails(!showDetails)}>
+					{showDetails ? 'hide' : 'view'}
+				</button>
+			</div>
+			{showDetails ? (
+				<div>
+					<p>{blog.url}</p>
+					<p>
+						<span>{blog.likes}</span>
+						<button type='button'>like</button>
+					</p>
+					<p>{blog.user.name}</p>
+				</div>
+			) : null}
+		</div>
+	);
+}
