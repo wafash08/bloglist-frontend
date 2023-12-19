9034ef39382105c43f9b0377704b0877aa92b507
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-19 14:40:40 +0700

    5.18 bloglist e2e testing step2

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,7 +1,12 @@
 describe('Blog app', function () {
-	// must empty the database
 	beforeEach(function () {
 		cy.request('POST', 'http://localhost:8080/api/testing/reset');
+		const user = {
+			name: 'John Dalton',
+			username: 'johndalton',
+			password: '1234',
+		};
+		cy.request('POST', 'http://localhost:8080/api/users/', user);
 		cy.visit('http://localhost:5173');
 	});
 
@@ -10,5 +15,21 @@ describe('Blog app', function () {
 		cy.get('[data-cy="username"]');
 		cy.get('[data-cy="password"]');
 		cy.get('[data-cy="login"]');
+	});
+
+	describe('Login', function () {
+		it('succeeds with correct credentials', function () {
+			cy.get('[data-cy="username"]').type('johndalton');
+			cy.get('[data-cy="password"]').type('1234');
+			cy.get('[data-cy="login"]').click();
+			cy.contains('John Dalton logged in');
+		});
+
+		it('fails with wrong credentials', function () {
+			cy.get('[data-cy="username"]').type('wrongusername');
+			cy.get('[data-cy="password"]').type('wrongpassword');
+			cy.get('[data-cy="login"]').click();
+			cy.contains('username or password is wrong');
+		});
 	});
 });
